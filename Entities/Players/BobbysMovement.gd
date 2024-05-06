61aeb15f6e95573956e6b85d60745538060a4f0a
Votline <quogg2@gmail.com>
2024-05-06 20:36:44 +0300

    Pre-Release MovementController[1]

--- Entities/Players/BobbysMovement.gd
+++ Entities/Players/BobbysMovement.gd
@@ -2,6 +2,7 @@ extends CharacterBody3D
 
 var direction; var input_dir; var isMoving: bool; var isRunning: bool; var isCrouching: bool; var isSliding: bool #Move
 var current_speed:float; var walk_speed:float; var run_speed: float; var crouch_speed: float #Speed
+var slide_direction: Vector3
 var gravity: float 
 
 
@@ -21,7 +22,7 @@ func _ready():
 	gravity = 9.8
 
 func _process(delta):
-	print(current_speed)
+	print(scale.y)
 	slide(delta)
 	move(delta)
 	crouch()
@@ -33,13 +34,16 @@ func move(delta):
 		
 	input_dir=Input.get_vector("move_left", "move_right", "move_forward", "move_back") #Get inputs
 	direction=(head.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized() #Making direction with character orientation
-	if direction:
-		isMoving=true; current_speed=lerp(current_speed, walk_speed, 0.02) #Smooth speed
-	else:
-		isMoving=false; current_speed=0
-	velocity.x = direction.x * current_speed #Move the character along X axis
-	velocity.z = direction.z * current_speed #Move the character along Z axis
-	move_and_slide()
+	if !isSliding:
+		if direction:
+			isMoving=true; 
+			if !isRunning && !isCrouching && !isSliding:
+				current_speed=lerp(current_speed, walk_speed, 0.02) #Smooth speed
+		else:
+			isMoving=false; current_speed=0
+		velocity.x = direction.x * current_speed #Move the character along X axis
+		velocity.z = direction.z * current_speed #Move the character along Z axis
+		move_and_slide()
 
 func run():
 	if !isSliding:
@@ -50,15 +54,18 @@ func run():
 func crouch():
 	if !isSliding:
 		if !isRunning && Input.is_action_pressed("crouch"):
+			scale.y = lerp(scale.y, 0.8, 0.09)
 			isCrouching=true; current_speed=lerp(current_speed, crouch_speed, 0.07)
-		if isCrouching && Input.is_action_just_released("crouch"):
-			isCrouching=false
+		if isCrouching && Input.is_action_just_released("crouch"): isCrouching=false
+		if !isCrouching && !isSliding:
+			scale.y = lerp(scale.y, 1.25, 0.04)
 func slide(delta):
-	if current_speed>9.7 && Input.is_action_pressed("crouch"): isSliding=true
+	if current_speed>9.5 && Input.is_action_pressed("crouch"): isSliding=true
 	if isSliding && current_speed > 3:
+		slide_direction = -camera.global_transform.basis.z.normalized(); slide_direction.y = direction.y
+		scale.y = lerp(scale.y, 0.5, 0.09)
+
 		current_speed=lerp(current_speed, crouch_speed, 0.01)
-		translate(Vector3.FORWARD * current_speed * delta)
+		translate(slide_direction * current_speed * delta)
 	else:
 		isSliding=false;
-func physics():
-	pass
