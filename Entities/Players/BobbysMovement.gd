7dd988dc83854955013fd058554c42942347ea3e
Votline <quogg2@gmail.com>
2024-05-08 00:16:05 +0300

    Pre-Release MovementController.v3(adding camera fov, headbob by x axis)[1]

--- Entities/Players/BobbysMovement.gd
+++ Entities/Players/BobbysMovement.gd
@@ -4,6 +4,7 @@ var slide_direction: Vector3; var scale_normal: float; var scale_crouch: float; 
 var direction; var input_dir; var isMoving: bool; var isRunning: bool; var isCrouching: bool; var isSliding: bool #Move
 var cb_frequency: float; var cb_amplitude: float; var cb_time: float; var cb_pos: Vector3 #Camera Bobbing
 var current_speed:float; var walk_speed:float; var run_speed: float; var crouch_speed: float #Speed
+var fov_normal: float; var fov_crouch: float; var fov_run: float #Camera Fov
 
 const gravity = 9.8 
 
@@ -22,6 +23,7 @@ func _ready():
 	isMoving=false; isRunning=false; isCrouching=false; isSliding=false; #Move
 	current_speed=0; walk_speed=5; run_speed=10; crouch_speed=2; #Speed
 	scale_normal=1.25; scale_crouch=0.8; scale_slide=0.5; #Scale
+	fov_normal=90; fov_crouch=60; fov_run=120; #Camera Fov 
 	cb_frequency=2.8; cb_amplitude=0.08; #Camera Bobbing
 
 
@@ -31,6 +33,8 @@ func _process(delta):
 	move(delta)
 	crouch()
 	run()
+	
+	if !isSliding && !isCrouching && !isRunning: camera.fov = fov_change(fov_normal) #Change the camera fov to 90
 
 func move(delta):
 	if not is_on_floor(): #if character not on floor
@@ -53,6 +57,7 @@ func run():
 	if !isSliding:
 		if isMoving && !isCrouching && Input.is_action_pressed("run"):
 			isRunning=true; current_speed=lerp(current_speed, run_speed, 0.07) #Smooth change speed to 10
+			camera.fov = fov_change(fov_run) #Change the camera fov to 120
 		if isRunning && Input.is_action_just_released("run"):
 			isRunning=false
 func crouch():
@@ -60,9 +65,11 @@ func crouch():
 		if !isRunning && Input.is_action_pressed("crouch"):  #Crouch
 			current_speed=lerp(current_speed, crouch_speed, 0.07) #Smooth change speed to 2
 			scale.y = lerp(scale.y, scale_crouch, 0.09) #Smooth change scale CharacterBody3D(player(Bobby)) to 0.8
-			isCrouching=true; #Change value
+			camera.fov = fov_change(fov_crouch) #Change the camera fov to 60
+			isCrouching=true
 		if isCrouching && Input.is_action_just_released("crouch"): isCrouching=false #Get up
 		if !isCrouching && !isSliding: scale.y = lerp(scale.y, scale_normal, 0.04) #Smooth change scale, is also works for slide
+		
 func slide(delta):
 	if current_speed>9.5 && Input.is_action_pressed("crouch"): isSliding=true
 	if isSliding && current_speed > 3:
@@ -70,6 +77,7 @@ func slide(delta):
 		current_speed=lerp(current_speed, crouch_speed, 0.01) #Smooth change speed to 2
 		translate(slide_direction * current_speed * delta) #moving the player in the direction for the camera
 		scale.y = lerp(scale.y, scale_slide, 0.09) #Smooth change scale to 0.5
+		camera.fov = fov_change(fov_run) #Change the camera fov to 120
 	else:
 		isSliding=false;
 		
@@ -77,5 +85,8 @@ func headbob(delta): # Function for simulating camera bobbing
 	cb_time += delta * velocity.length() * float(is_on_floor()) # Increase the bobbing time based on the velocity and whether the camera is on the floor
 	cb_pos = Vector3.ZERO # Reset the camera position vector
 	cb_pos.y = sin(cb_time * cb_frequency) * cb_amplitude # Calculate the vertical position of the camera using a sine function
+	cb_pos.x = cos(cb_time * cb_frequency/2) * cb_amplitude # Calculate the horizontal position of the camera using a sine function
 	camera.transform.origin = cb_pos # Update the camera position
 
+func fov_change(fov_new) -> float: #Func for change the camera fov
+	return lerp(camera.fov, fov_new, 0.03) #Return value for change camera fov
